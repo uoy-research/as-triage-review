c1ef9862fba9c5ae40353db62ebb2f716de5b10f
bilyons <billylyons1710@gmail.com>
2022-10-26 13:10:20 +0100

    web page updates

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -12,12 +12,8 @@ import pandas as pd
 from pip import main
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-# Permanently changes the pandas settings
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', -1)
+from upsetplot import plot
+sns.set(rc={'savefig.dpi':200})
 
 current_directory = os.path.abspath(os.getcwd())
 
@@ -42,27 +38,125 @@ def dataPointAnalysis():
 
     print(f"Outliers for Retrospective: {outliersRetrospective}")
     print(f"Outliers for Retrospective: {outliersevp}")
-    print(f"Outliers for Retrospective: {outlierslTrials}")   
-    exit()
-    
-def stage5Analaysis():
+    print(f"Outliers for Retrospective: {outlierslTrials}")
+
+def illnessComparison():
+    # Load respective .csv file
+    df = pd.read_csv(current_directory + "/csvFiles/dataPointsAndTypes.csv")
+    # Delete all unncessary coiumns
+    df.drop(['Author', 'Title', 'Year', 'S or V', 'Patient Facing', 'Regular Use', 'Age', 'Notes', 'EV&P Data', 'Live Trials', 'Retrospective', 'Unnamed: 11', 'Unnamed: 13'], axis=1, inplace=True)
+    to_groupby = list(df.columns.values)
+    # Groupby and count data
+    grouped = df.groupby(to_groupby).size().sort_values(ascending=False).reset_index(name='Counts')
+    sns.barplot(data=grouped, x="Illness", y="Counts")
+    plt.xticks(rotation=45, horizontalalignment='right')
+    plt.xlabel('Illness considered', fontweight='bold')
+    plt.ylabel('Counts', fontweight='bold')
+    plt.tight_layout()
+    plt.show()
+
+def dataCombinationsAnalysis():
+    # Load respective .csv file
+    df = pd.read_csv(current_directory + "/csvFiles/dataPointsAndTypes.csv")
+    # Delete all unncessary coiumns
+    df.drop(['Author', 'Title', 'Year', 'Patient Facing', 'Regular Use', 'Age', 'Notes', 'Illness', 'Unnamed: 11', 'Unnamed: 13'], axis=1, inplace=True)
+    df = df.mask(df>0,1)
+    to_groupby = list(df.columns.values)
+    # Groupby and count data
+    grouped = df.groupby(to_groupby).size().sort_values(ascending=False)
+    # Plot upset plot
+    plot(grouped, show_counts=True)
+    plt.show()
+
+def stage5Analysis():
     # Load respective .csv file
     df = pd.read_csv(current_directory + "/csvFiles/stage5Data.csv")
-    
-    pass
-def stagesAnalysis():
-    # Load respective .csv file
-    df = pd.read_csv(current_directory + "csvFiles/stagesData.csv")
-    
-    pass
-def technologiesAnalysis():
-    # Load respective .csv file
-    df = pd.read_csv(current_directory + "csvFiles/technologiesData.csv")
+    # Delete unnecessary columns
+    df.drop(['Author','Title', 'Unnamed: 26'], axis=1, inplace=True)
+    to_groupby = list(df.columns.values)
+    # Set NaN to 0
+    df = df.fillna(0)
+    # Groupby and count data
+    grouped = df.groupby(to_groupby).size().sort_values(ascending=False)
+    # Plot upset plot
+    plot(grouped, show_counts=True)
+    plt.show()   
     pass
 
+def stagesAnalysis():
+    # Load respective .csv file
+    df = pd.read_csv(current_directory + "/csvFiles/stagesData.csv", skipfooter=2)
+    # Delete unnecessary columns
+    df.drop(['Analysis - Stages', 'Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 18'], axis=1, inplace=True)
+    # Skip first two rows
+    df = df.iloc[1:]
+    # Reset column headers
+    new_header = df.iloc[0] #grab the first row for the header
+    df = df[1:] #take the data less the header row
+    df.columns = new_header #set the header row as the df header
+    # Drop extraneous columns
+    df.drop(['Author', 'Title', 'Year','which system'], axis=1, inplace=True)
+    # Set occupied to 1
+    df = df.notna().astype(int)
+    # Set NaN to 0
+    df = df.fillna(0)
+    # Groupby and count data
+    grouped = df.groupby(['0','1','2','3','4','5']).size().sort_values(ascending=False)
+    # Plot upset plot
+    plot(grouped, show_counts=True)
+    plt.show()
+    pass
+
+def technologiesAnalysis():
+    # Load respective .csv file
+    df = pd.read_csv(current_directory + "/csvFiles/technologiesData.csv")
+    total_tech = df[["Technology", "Count"]]
+    # Plot top 10 largest techs
+    sns.barplot(data=total_tech.nlargest(10, 'Count'), x="Technology", y = "Count")
+    plt.xticks(rotation=45, horizontalalignment='right')
+    plt.xlabel('Technology', fontweight='bold')
+    plt.ylabel('Counts', fontweight='bold')
+    plt.tight_layout()
+    pass
+
+def technologiesComboAnalysis():
+    # Load respective .csv file
+    df = pd.read_csv(current_directory + "/csvFiles/technologiesData.csv")
+    total_tech = df[["Technology", "Count"]]
+    # Plot top 10 largest techs
+    sns.barplot(data=total_tech.nlargest(10, 'Count'), x="Technology", y = "Count")
+    plt.xticks(rotation=45, horizontalalignment='right')
+    plt.xlabel('Technology', fontweight='bold')
+    plt.ylabel('Counts', fontweight='bold')
+    plt.tight_layout()
+    plt.show()
+    pass
+
+def mostSuccessfulResults():
+    # Load respective .csv file
+    df = pd.read_csv(current_directory + "/csvFiles/hardwareAndMostSuccessful.csv", skipfooter=9)
+    # Delete unnecessary columns
+    df.drop(['Technology', 'Count', 'Unnamed: 2', 'Hardware', 'Hardware Count', 'Unnamed: 5', 'Author', 'Title', 'Unnamed: 8'], axis=1, inplace=True) 
+    to_groupby = list(df.columns.values)
+    # Groupby and count data
+    grouped = df.groupby(to_groupby).size().sort_values(ascending=False).reset_index(name='Counts')
+    print(grouped)
+    sns.barplot(data=grouped, x="Best", y="Counts")
+    plt.xticks(rotation=45, horizontalalignment='right')
+    plt.xlabel('Reportedly most successful algorithm or combination', fontweight='bold')
+    plt.ylabel('Counts', fontweight='bold')
+    plt.tight_layout()
+    plt.show()
+
 def main():
-    dataPointAnalysis()
-    
+    # illnessComparison()
+    # dataPointAnalysis()
+    dataCombinationsAnalysis()
+    stagesAnalysis()
+    stage5Analysis()
+    # technologiesAnalysis()
+    # technologiesComboAnalysis()
+    # mostSuccessfulResults()
 
 if __name__ == "__main__":
     main()
